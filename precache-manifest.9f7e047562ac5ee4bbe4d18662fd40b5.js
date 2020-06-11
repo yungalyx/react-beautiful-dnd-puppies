7e45a1327759d7d87ec950504f983d4ccc635667
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-06-10 18:13:25 -0700

    Updates

---
+++ precache-manifest.9f7e047562ac5ee4bbe4d18662fd40b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d409e086339c5507bccdb3d6ca9c22",
+    "url": "/yungalyx/react-beautiful-dnd-puppies-1/index.html"
+  },
+  {
+    "revision": "31b7babd222ad9c29146",
+    "url": "/yungalyx/react-beautiful-dnd-puppies-1/static/css/main.fa969550.chunk.css"
+  },
+  {
+    "revision": "7418b71d204af5dd7380",
+    "url": "/yungalyx/react-beautiful-dnd-puppies-1/static/js/2.9e40f091.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/yungalyx/react-beautiful-dnd-puppies-1/static/js/2.9e40f091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b7babd222ad9c29146",
+    "url": "/yungalyx/react-beautiful-dnd-puppies-1/static/js/main.4f9b4722.chunk.js"
+  },
+  {
+    "revision": "c13634fca20c8c406aea",
+    "url": "/yungalyx/react-beautiful-dnd-puppies-1/static/js/runtime-main.922efe10.js"
+  }
+]);
